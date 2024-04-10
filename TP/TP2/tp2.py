3cb8cf0baecf50babf660e923560253a4f8f20b4
julien pontoire <julien.pontoire@etu.utc.fr>
2024-04-10 23:46:48 +0200

    affichage

--- TP/TP2/tp2.py
+++ TP/TP2/tp2.py
@@ -1,5 +1,8 @@
 """Code de l'exercice 3 du TP2 de IA02"""
+
 from typing import List
+import subprocess
+import math
 
 
 def at_least_one(s):
@@ -22,35 +25,35 @@ def at_most_one(s):
 def contraintes(t):
     "définit les contraintes entre les sommets"
     l: List = []
-    for i in range(len(t)):
+    for _, i in enumerate(t):
         # print(-((t[i][0]-1)*3+1))
-        l.append([-((t[i][0] - 1) * 3 + 1), -((t[i][1] - 1) * 3 + 1)])
-        l.append([-((t[i][0] - 1) * 3 + 2), -((t[i][1] - 1) * 3 + 2)])
-        l.append([-((t[i][0] - 1) * 3 + 3), -((t[i][1] - 1) * 3 + 3)])
+        l.append([-((i[0] - 1) * 3 + 1), -((i[1] - 1) * 3 + 1)])
+        l.append([-((i[0] - 1) * 3 + 2), -((i[1] - 1) * 3 + 2)])
+        l.append([-((i[0] - 1) * 3 + 3), -((i[1] - 1) * 3 + 3)])
     # print(len(l))
     return l
 
 
 def graph(c, n, s, a):
     """définit fichier cnf"""
-    cnf = ""
+    cnf: str = ""
     cnf += "c tp2.cnf\nc\n"
     l: List = []
-    for i in range(len(s)):
+    for i, _ in enumerate(s):
         l.append(at_least_one(i + 1))
         tmp: List = at_most_one(i + 1)
-        for i in range(len(tmp)):
-            l.append(tmp[i])
+        for i in tmp:
+            l.append(i)
     tmp2: List = contraintes(a)
-    for i in range(len(tmp2)):
-        l.append(tmp2[i])
+    for _, tmp2_i in enumerate(tmp2):
+        l.append(tmp2_i)
     # print(len(l))
     # print(l)
 
     cnf += f"p cnf {c*n} {len(l)}\n"
-    for i in range(len(l)):
-        for j in range(len(l[i])):
-            cnf += f"{l[i][j]} "
+    for _, i in enumerate(l):
+        for _, j in enumerate(i):
+            cnf += f"{j} "
         cnf += "0\n"
     cnf += "\n"
 
@@ -60,10 +63,40 @@ def graph(c, n, s, a):
     print(cnf)
 
 
+def affichage(path: str) -> None:
+    """Affichage du résultat"""
+    colors: dict = {0: "bleu", 1: "vert", 2: "rouge"}
+    process = subprocess.run(
+        f"/home/jpontoire/go/bin/gophersat {path}",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True,
+    )
+
+    returned_values: str = process.stdout
+    temp: list = returned_values.split("\n")
+    results = temp[2].replace("v", "").split(" ")
+    results.remove("")
+    results.pop(len(results) - 1)
+    # print(results)
+    sommets_colors = {}
+    for item in results:
+        number = int(item)
+        if number > 0:
+            sommet: int = math.ceil(number / 3)
+            color: str = colors[int(number % 3)]
+            sommets_colors[sommet] = color
+    # print(sommets_colors)
+    for cle, valeur in sommets_colors.items():
+        print(f"Sommet {cle} : {valeur}")
+
+
 def main() -> None:
     """fontion main"""
-    c = 3
-    n = 10
+    c: int = 3
+    n: int = 10
     sommets: List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     arretes: List = [
         (1, 2),
@@ -83,6 +116,7 @@ def main() -> None:
         (8, 10),
     ]
     graph(c, n, sommets, arretes)
+    affichage("/home/jpontoire/Documents/UTC/GI02/IA02/TP/TP2/tp2.cnf")
 
 
 if __name__ == "__main__":
